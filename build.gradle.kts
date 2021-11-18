6d33ba986094d7208dda91658473f9a1a218fd99
Hiroshi Miura <miurahr@linux.com>
2021-11-18 16:50:26 +0900

    Set target compatibility to Java 8
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,6 +67,7 @@ tasks.withType<JavaCompile> {
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
     withSourcesJar()
     withJavadocJar()
 }
